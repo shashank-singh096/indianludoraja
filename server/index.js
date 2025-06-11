18d8b9e7bb6c7b8ea83158055a81bbc67efb32df
shashank-singh096 <shivamshkumar.hjp999@gmail.com>
2025-06-11 14:45:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,30 +7,46 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+const rooms = {};
 
-  socket.on("createRoom", (room) => {
-    socket.join(room);
-    console.log(`Room created: ${room}`);
-    socket.emit("message", `Room '${room}' created. Waiting for another player...`);
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("createRoom", (roomId) => {
+    rooms[roomId] = [socket.id];
+    socket.join(roomId);
+    socket.emit("roomJoined", roomId);
   });
 
-  socket.on("joinRoom", (room) => {
-    socket.join(room);
-    console.log(`User joined room: ${room}`);
-    io.to(room).emit("message", `Both players joined Room '${room}'. Game will start!`);
+  socket.on("joinRoom", (roomId) => {
+    if (rooms[roomId] && rooms[roomId].length === 1) {
+      rooms[roomId].push(socket.id);
+      socket.join(roomId);
+      socket.emit("roomJoined", roomId);
+      io.to(roomId).emit("gameStart", roomId);
+    } else {
+      socket.emit("roomJoined", "Room full or invalid");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (const room in rooms) {
+      rooms[room] = rooms[room].filter((id) => id !== socket.id);
+      if (rooms[room].length === 0) {
+        delete rooms[room];
+      }
+    }
+    console.log("User disconnected:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
